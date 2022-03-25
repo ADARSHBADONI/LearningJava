23ce274be7c382e5c5b6b3bd7b0e146a4612c710
gehu <gehu@localhost>
2022-03-25 16:32:38 +0530

    COMMIT1

--- abc/src/UNIT_01/P2_Task01_KeywordsInJava.java
+++ abc/src/UNIT_01/P2_Task01_KeywordsInJava.java
@@ -5,7 +5,7 @@ public class P2_Task01_KeywordsInJava {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
  int double=12;
- //
+ //keywords can never be use as name
 	}
 
 }

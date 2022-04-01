af9de7ef849615e5a8072445113bdf313255e37b
gehu <gehu@localhost>
2022-04-01 16:12:40 +0530

    commit

--- Unit01/src/UNIT_01/P1_Task01_helloworld.java
+++ Unit01/src/UNIT_01/P1_Task01_helloworld.java
@@ -3,7 +3,8 @@ package UNIT_01;
 public class P1_Task01_helloworld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("hello world\n");
 
 	}
 

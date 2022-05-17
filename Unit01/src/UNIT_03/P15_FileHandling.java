368080b4f688cf8179b5b2a59b6b2b20c2179563
minato <minato@adarsh-pc>
2022-05-17 21:34:37 +0530

    commit

---
+++ Unit01/src/UNIT_03/P15_FileHandling.java
@@ -0,0 +1,33 @@
+package UNIT_03;
+public class P15_FileHandling {
+
+	public static void main(String[] args) {
+		
+		
+		FileHandlingInJava obj= new FileHandlingInJava();
+		obj.takeInputFromAFileNotFromConsole();
+		obj.saveOutputIntoFile();
+		obj.createALogFile();
+		
+
+	}
+
+}
+
+class FileHandlingInJava
+{
+	void takeInputFromAFileNotFromConsole()
+	{
+		
+	}
+	
+	void saveOutputIntoFile()
+	{
+		
+	}
+	
+	void createALogFile()
+	{
+		
+	}
+}
---
+++ Unit01/src/UNIT_03/P16_Multithreading.java
@@ -0,0 +1,43 @@
+package UNIT_03;
+public class P16_Multithreading {
+	public static void main(String[] args)
+	{
+		
+		MultiThreadingInJava obj = new MultiThreadingInJava();
+		obj.threadConcept();
+		obj.threadSynchronization();
+		obj.interThreadCommunication();
+		obj.ProducerConsumerProblems();
+		obj.Wait_And_Notify();
+	}
+}
+
+
+class MultiThreadingInJava
+{
+void threadConcept()
+{
+	
+}
+
+void threadSynchronization()
+{
+	
+}
+
+void interThreadCommunication()
+{
+	
+}
+
+void ProducerConsumerProblems()
+{
+	
+}
+
+void Wait_And_Notify()
+{
+	
+}
+}
+

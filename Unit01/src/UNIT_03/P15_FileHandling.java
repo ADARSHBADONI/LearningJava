037869d765bbc691bd2edb33e847e3f92a808436
minato <minato@adarsh-pc>
2022-05-18 09:10:30 +0530

    a

--- Unit01/src/UNIT_03/P15_FileHandling.java
+++ Unit01/src/UNIT_03/P15_FileHandling.java
@@ -1,33 +1,40 @@
 package UNIT_03;
+import java.io.BufferedReader;
+import java.io.File;
+
 public class P15_FileHandling {
 
 	public static void main(String[] args) {
-		
-		
-		FileHandlingInJava obj= new FileHandlingInJava();
+		FileHandlingInJava obj = new FileHandlingInJava();
 		obj.takeInputFromAFileNotFromConsole();
-		obj.saveOutputIntoFile();
-		obj.createALogFile();
-		
+	//	obj.saveOutputIntoFile();
+	//	obj.createALogFile();
 
 	}
-
+	
 }
 
-class FileHandlingInJava
-{
-	void takeInputFromAFileNotFromConsole()
-	{
+class FileHandlingInJava{
+	void takeInputFromAFileNotFromConsole() {
+		String path = "java.txt";
+		File file = new File(path);
+		BufferedReader br = new BufferedReader(new FileReader(file));
+		
+		String st;
+		while((st = br.readLine()) != null) {
+			System.out.println(st);
+		}
+		
+//		List that holds strings of a file
+		List<Stirng> listOfString = new ArrayList<Stirng>();
+		
+		String line = br.readLine();
+	}
+	void saveOutputIntoFile() {
+		
+    }
+	void createALogFile() {
 		
 	}
-	
-	void saveOutputIntoFile()
-	{
-		
-	}
-	
-	void createALogFile()
-	{
-		
-	}
+
 }
--- Unit01/src/UNIT_03/Sample.java
+++ Unit01/src/UNIT_03/Sample.java
@@ -1,10 +1,26 @@
 package UNIT_03;
+//finally would run anyway
+
+import java.io.IOException;
+import java.util.InputMismatchException;
 
 public class Sample {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		try {
+			int a = 100/0;
+			System.out.println("pritn value of a");
+		}catch (InputMismatchException e){
+			
+			System.out.println("Arithmetic ");
+		}
+//       catch (Exception e){
+//			
+//			System.out.println("General exception );
+//		}
+		finally{      // if there is no exception still finally would always run 
+		System.out.println("done");
+		}
 	}
-
+//if there is exception in try block and there is no appropriate  catch block still finally would run  
 }

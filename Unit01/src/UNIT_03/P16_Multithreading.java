d1bbc7a04cfb7dda825571f19e8e705fba563a9f
minato <minato@adarsh-pc>
2022-05-25 09:50:13 +0530

    commit

--- Unit01/src/UNIT_03/P16_Multithreading.java
+++ Unit01/src/UNIT_03/P16_Multithreading.java
@@ -1,56 +1,60 @@
 package UNIT_03;
-
 /*
- * MultiProcessing:
- * -Sequencial MultiProcessing
- * -Parallel MultiProcessing
- * -Concurrent Multiprocessing
- * 
- * MultiTasking:
- * -Sequencial MultiTasking
- * -Parallel MultiTasking (Not Applicable)
- * -Concurrent MultiTasking
- * 
- */
-
+ * prerequisite
+ * MultiProcessing-
+ *  1. sequential multiprocessing
+ *  2. parallel multiprocessing
+ *  3. concurrent multiprocessing
+ * MultiTasking-
+ *  1. sequential multitasking
+ *  2. parallel multitasking
+ *  3. concurrent multitasking
+ * */
+import java.util.*;
 public class P16_Multithreading {
-		public static void main(String[] args)
-		{
-			
-			MultiThreadingInJava obj = new MultiThreadingInJava();
-			obj.threadConcept();
-			obj.threadSynchronization();
-			obj.interThreadCommunication();
-			obj.ProducerConsumerProblems();
-			obj.Wait_And_Notify();
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		c1 obj1 = new c1();
+		c2 obj2 = new c2();
+		obj1.show1();
+		obj2.show2();
+//		Thread t1 = new Thread(new t1(),"T1");
+//		Thread t2= new Thread(new t2(),"T2");
+//		t1.start();
+//		t2.start();
+
 		}
-}
+	}
 
-
-class MultiThreadingInJava
-{
-	void threadConcept()
-	{
-		
-	}
-	
-	void threadSynchronization()
-	{
-		
-	}
-	
-	void interThreadCommunication()
-	{
-		
-	}
-	
-	void ProducerConsumerProblems()
-	{
-		 
-	}
-	
-	void Wait_And_Notify()
-	{
+class c1{
+	void show1() {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		System.out.println(+a);
 		
 	}
 }
+class c2{
+	void show2() {
+		for(int i=0;i<10;i++) {
+			System.out.println("hey ");
+		}
+	}
+}
+
+//class t1 extends Thread{
+//	public void run() {
+//		Scanner sc = new Scanner(System.in);
+//		int a = sc.nextInt();
+//		System.out.println(+a);
+//		
+//	}
+//}
+//class t2 extends Thread{
+//	public void run() {
+//		for(int i=0;i<10;i++) {
+//			System.out.println("hey ");
+//		}
+//	}
+//}
